49e9de7b66752ad8f2f7fc3d5d65f2e109a032b0
Pranav-Bobde <bobdep31@gmail.com>
2021-11-10 02:24:15 +0530

    removed auth-header logs

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,8 +2,6 @@ export default function authHeader() {
     let user = JSON.parse(localStorage.getItem('user'));
   
     if (user && user.token) {
-      console.log("true");
-      console.log(user.token);
       return { Authorization: 'Bearer ' + user.token }; 
       // return { 'x-access-token': user.accessToken };      
     } else {
