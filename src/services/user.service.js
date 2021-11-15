d3c69c8396d4cd00958bab6a301150b8ceaa23d0
Pranav-Bobde <bobdep31@gmail.com>
2021-11-15 13:52:53 +0530

    removed the cors-anywhere

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-const API_URL = 'https://cors-anywhere.herokuapp.com/https://ekatra-backend-mvp-1.herokuapp.com/';
+const API_URL = 'https://ekatra-backend-mvp-1.herokuapp.com/';
 // const API_URL = 'http://127.0.0.1:5000/api/';
 
 class UserService {
